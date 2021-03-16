ce9933454f52b273c3a8e3d1dbde69e3db7e5c41
Darshit Rudani <darshit112001@users.noreply.github.com>
2021-03-16 21:56:10 +0530

    add body design

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  Expanded buildcode({Color color, int soundnumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {},
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-    )
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildcode(color: Colors.red),
+              buildcode(color: Colors.orange),
+              buildcode(color: Colors.yellow),
+              buildcode(color: Colors.green),
+              buildcode(color: Colors.teal),
+              buildcode(color: Colors.blue),
+              buildcode(color: Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
